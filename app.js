39ab6719972ed817cb709750c2f5b699a6195517
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-16 12:54:29 +0300

    Setup route for fetching tours

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+// Import Express
 const express = require('express');
+
+// Import dotenv to load environment variables
 const dotenv = require('dotenv');
 
+// Load environment variables
 dotenv.config();
 
+// Initialize Express app
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('Hello from the server');
+// Retrieve and send all tour data with a success message
+app.get('/api/v1/tours', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Tours retrieved successfully!',
+    data: { tours: '<all tours>' },
+  });
 });
 
+// Start the server on the specified port or default to 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server up and running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
