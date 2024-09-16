a19085e8e509b486e79219c4582fd679c4fa7c25
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-16 13:17:22 +0300

    separate the routes handlers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Import Express framework for building the server
+// Import the Express framework for building the server
 const express = require('express');
 
 // Import dotenv to manage environment variables
@@ -10,50 +10,64 @@ dotenv.config();
 // Initialize an instance of an Express application
 const app = express();
 
-// Route handler for GET All tours   /api/v1/tours
-app.get('/api/v1/tours', (req, res, next) => {
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// get All tours handler
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     message: 'Tours retrieved successfully!',
     data: { tours: '<all tours>' },
   });
-});
+};
 
-// Route handler for POST new tour requests to /api/v1/tours
-app.post('/api/v1/tours', (req, res, next) => {
+const createTour = (req, res) => {
   res.status(201).json({
     status: 'success',
     message: 'New tour created successfully!',
-    data: { tours: '<new tours>' },
+    data: { tour: '<new tour>' },
   });
-});
+};
 
-// Route handler for GET requests to /api/v1/tours/:id
-app.get('/api/v1/tours/:id', (req, res, next) => {
+const getTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     message: 'Tour retrieved successfully!',
-    data: { tours: `<tour: ${req.params.id}>` },
+    data: { tour: `<tour: ${req.params.id}>` },
   });
-});
+};
 
-// Route handler for update single Tour   /api/v1/tours/:id
-app.patch('/api/v1/tours/:id', (req, res, next) => {
+const updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     message: 'Tour updated successfully!',
-    data: { tours: `<tour: ${req.params.id}>` },
+    data: { tour: `<tour: ${req.params.id}>` },
   });
-});
+};
 
-// Route handler for GET single Tour   /api/v1/tours/:id
-app.delete('/api/v1/tours/:id', (req, res, next) => {
+const deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
     message: 'Tour deleted successfully!',
-    data: { tours: `<tour: ${req.params.id}>` },
+    data: null, // No content to return for DELETE request
   });
-});
+};
+
+// Route handler to GET all tours
+app.get('/api/v1/tours', getAllTours);
+
+// Route handler for CREATE a new tour
+app.post('/api/v1/tours', createTour);
+
+// Route handler for GET single tour
+app.get('/api/v1/tours/:id', getTour);
+
+// Route handler for UPDATE a tour
+app.patch('/api/v1/tours/:id', updateTour);
+
+// Route handler for DELETE a tour
+app.delete('/api/v1/tours/:id', deleteTour);
 
 // Start the server on the specified port from environment variables or default to 5000
 const PORT = process.env.PORT || 5000;
