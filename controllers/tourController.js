a8447952fa1a529ba081d9ff6aa2fa6b164dd39d
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-17 10:54:53 +0300

    add: get all tours features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,12 +1,22 @@
 const Tour = require('./../models/tourModel');
 
 // Handler to get all tours
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Tours retrieved successfully!',
-    data: { tours: '<all tours>' },
-  });
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      length: tours.length,
+      message: 'Tours retrieved successfully!',
+      data: { tours: tours },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.messge,
+      error: err,
+    });
+  }
 };
 
 // Handler to create a new tour
@@ -29,12 +39,23 @@ const createTour = async (req, res) => {
 };
 
 // Handler to get a single tour by ID
-const getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    message: 'Tour retrieved successfully!',
-    data: { tour: `<tour: ${req.params.id}>` },
-  });
+const getTour = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const tour = await Tour.findById(id);
+
+    res.status(201).json({
+      status: 'success',
+      message: 'Tour retrieved successfully!',
+      data: { tour: tour },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.messge,
+      error: err,
+    });
+  }
 };
 
 // Handler to update a tour by ID
