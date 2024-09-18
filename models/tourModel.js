4de68f9f77b0329286536c6f39ad7ee00886b9da
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-18 10:28:48 +0300

    create a slug using document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,76 +1,93 @@
 const mongoose = require('mongoose');
-const { select } = require('underscore');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a price'],
+    },
+
+    ratingsAverage: {
+      type: Number,
+      default: 0,
+    },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+
+    priceDiscount: Number,
+
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+
+    images: [String],
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+
+    startDates: [Date],
+
+    slug: String,
   },
+  {
+    // this will add id property to the document also
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a price'],
-  },
-
-  ratingsAverage: {
-    type: Number,
-    default: 0,
-  },
-
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-
-  priceDiscount: Number,
-
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-
-  description: {
-    type: String,
-    trim: true,
-  },
-
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-
-  images: [String],
-
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-
-  startDates: [Date],
-});
-
+// Virtual Properties
 // Virutal properties: works each time we get data from the database
 tourSchema.virtual('durationInWeeks').get(function () {
   return this.duration / 7;
+});
+
+// Document Middleware: runs before .save() and .create() not on insertMany
+tourSchema.pre('save', function (next) {
+  // this refers to current processing document and that is why we call it document middleware
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
