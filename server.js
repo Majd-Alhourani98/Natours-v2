265504dd6152abb8c72e74a13e0ab87276161f77
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-18 22:55:41 +0300

    handle uncaught excrption

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv').config();
 // Import mongoose for database connection
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', err => {
+  console.log('Uncaught Exception 💥 Shutting down...');
+  console.log(err.name, err.message);
+  console.log(err);
+});
+
 // Import the Express application from the app module
 const app = require('./app');
 
@@ -12,8 +18,10 @@ const app = require('./app');
 const DATABASE_LOCAL_URL = process.env.DATABASE_LOCAL_URL;
 
 // Connect to the MongoDB database using Mongoose
-mongoose.connect(DATABASE_LOCAL_URL).then(conn => console.log('Database connection successful'));
-// .catch(err => console.log('Failed to connect to the database 🔥', err));
+mongoose
+  .connect(DATABASE_LOCAL_URL)
+  .then(conn => console.log('Database connection successful'))
+  .catch(err => console.log('Failed to connect to the database 🔥', err));
 
 // Start the server on the specified port from environment variables or default to 5000
 // Retrieve the port number from environment variables
@@ -24,7 +32,7 @@ const server = app.listen(PORT, () => {
 
 // Unhandle Rejection
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION 💥 Shutting down...');
+  console.log(err);
   server.close(() => process.exit(1));
 });
