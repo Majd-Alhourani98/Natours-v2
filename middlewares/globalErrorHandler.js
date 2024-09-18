a332924d0c92bc802f8180ef6d9a5bc5d7519ec6
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-18 18:57:22 +0300

    distinguish errors between development and production

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,9 +1,36 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  console.log('ERROR 🔥', err);
+  res.status(500).json({
+    status: 'error',
+    message: 'Something went very worng',
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
   // send error response
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  console.log(process.env.NODE_ENV);
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
