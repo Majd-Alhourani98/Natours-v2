c741502c756e9da8787bd6af0b80d30cd0ee2606
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-19 11:40:54 +0300

    create the sign up handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,14 @@ const APIFeatures = require('./../utils/APIFeatures');
 
 const catchAsync = require('../utils/catchAsync');
 
-const signup = catchAsync(async (req, res, next) => {});
+const signup = catchAsync(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    message: 'account created successfully',
+    data: { user: user },
+  });
+});
 
 module.exports = { signup };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -28,7 +29,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm you password'],
+    validate: {
+      // This only create on `.save()` and `.create()`
+      validator: function (val) {
+        return val === this.password;
+      },
+
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
