d07f24dc4c91879943464ae7c7e82de2c9d056a1
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-22 11:23:50 +0300

    modelling tour guides: child referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -131,6 +132,15 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+
+    // guides: Array,
+
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     // this will add `id` property to the document also
@@ -168,6 +178,12 @@ tourSchema.pre('aggregate', function (next) {
 
   next();
 });
+
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
